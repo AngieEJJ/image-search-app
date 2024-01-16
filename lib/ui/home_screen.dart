23cf6134e044935d03254fd490b51cdf066fc1e9
eunjinjeong <eunjin.jeong.5115@gmail.com>
2024-01-16 23:42:11 +0900

    feat: fetch 함수 UI 연결, textcontroller 추가

--- lib/ui/home_screen.dart
+++ lib/ui/home_screen.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:image_search/model/photo.dart';
 import 'package:http/http.dart' as http;
@@ -13,6 +12,10 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final _controller = TextEditingController();
+
+  List<Photo> _photos = [];
+
   Future<List<Photo>> fetch(String query) async {
     final response = await http.get(Uri.parse(
         'https://pixabay.com/api/?key=41487259-59b1e614313376c6d9c201e9c&q=$query&image_type=photo'));
@@ -25,8 +28,14 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   void initState() {
-    fetch('query');
+    fetch('rose');
     super.initState();
   }
 
@@ -42,6 +51,7 @@ class _HomeScreenState extends State<HomeScreen> {
         child: Column(
           children: [
             TextField(
+              controller: _controller,
               decoration: InputDecoration(
                 enabledBorder: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(10.0),
@@ -52,7 +62,12 @@ class _HomeScreenState extends State<HomeScreen> {
                 ),
                 hintText: 'Search',
                 suffixIcon: GestureDetector(
-                  onTap: () {},
+                  onTap: () async {
+                    final photos = await fetch(_controller.text);
+                    setState(() {
+                      _photos = photos;
+                    });
+                  },
                   child: const Icon(
                     Icons.search,
                     color: Color(0xFF4FB6B2),
@@ -63,14 +78,17 @@ class _HomeScreenState extends State<HomeScreen> {
             const SizedBox(height: 24),
             Expanded(
               child: GridView.builder(
-                  itemCount: 10,
+                  itemCount: _photos.length,
                   gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                     crossAxisCount: 2,
                     crossAxisSpacing: 32,
                     mainAxisSpacing: 32,
                   ),
                   itemBuilder: (context, index) {
-                    return Container();
+                    final photo = _photos[index];
+                    // print(_photos[index]);
+                    // print(_photos);
+                    return PhotoWidget(photo: photo);// list가 아닌 photo 타입이어야 하기 때문에 리스트가 아닌 해당하는 객체가 필요.
                   }),
             ),
           ],
